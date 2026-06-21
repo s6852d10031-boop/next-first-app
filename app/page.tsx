4694e565384cb39de9e9078b4ccf510921125446
s6852d10031-boop <s6852d10031@sau.ac.th>
2026-06-21 13:05:22 +0700

    เสร็จแล้ว99%%%

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Page() {
   return (
     <div>
       Hello....Main Page
+      <br  />
+      ID: 6852D10031
+      <br  />
+      Name: Patcha Suthirak
     </div>
   );
 }
